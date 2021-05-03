42ab25f3cc36492e0bdd239a7d6aec3778dfa268
yatsun <yatsunlam25@gmail.com>
2021-05-03 01:22:09 -0400

    commit tree f759b140da46d1b662e0c67fa9168be12d0edb69

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,19 @@
+# db/migrate/01_create_artists.rb
+
+class CreateArtists < ActiveRecord::Migration[4.2]
+    def change
+        create_table :artists do |t|
+            t.string :name
+            t.string :genre
+            t.integer :age
+            t.string :hometown
+        end
+    end
+
+    def up
+    end
+
+    def down
+    end
+
+end
